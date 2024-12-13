f8969ebff9af9981c0fd280aa0a59069d8833a09
Erick Lescano <eol99@protonmail.com>
2024-12-13 12:58:59 -0500

    Improve day 11 solution ocaml 2024

--- ocaml24/eleven.ml
+++ ocaml24/eleven.ml
@@ -1,83 +1,67 @@
 open Base
 open Stdio
 
-let rec split_even acc idx len string =
-  if idx = String.length string
-  then List.rev acc
-  else (
-    let substr = String.sub string ~pos:idx ~len in
-    let shortened = Int.to_string (Int.of_string substr) in
-    split_even (shortened :: acc) (idx + len) len string)
-;;
-
-let rec prepend left right =
-  match left with
-  | head :: tail -> prepend tail (head :: right)
-  | [] -> right
-;;
-
-let rec change_stones acc = function
-  | stone :: tail ->
-    if String.equal stone "0"
-    then change_stones ("1" :: acc) tail
-    else if String.length stone % 2 = 0
-    then (
-      let new_stones = split_even [] 0 (String.length stone / 2) stone in
-      change_stones (prepend new_stones acc) tail)
-    else (
-      let new_stone = Int.to_string (Int.of_string stone * 2024) in
-      change_stones (new_stone :: acc) tail)
-  | [] -> List.rev acc
-;;
-
-let part_one stones =
-  let final_stones =
-    Fn.apply_n_times ~n:25 (fun x -> change_stones [] x) stones
+let split_stone stone =
+  let len = String.length stone / 2 in
+  let left, right =
+    String.sub stone ~pos:0 ~len, String.sub stone ~pos:len ~len
   in
-  List.length final_stones
+  Int.of_string left, Int.of_string right
+;;
+
+let get_or_default map key = Option.value ~default:0 (Map.find map key)
+
+let blink stones =
+  let init = Map.empty (module Int) in
+  let created =
+    Map.fold stones ~init ~f:(fun ~key:stone ~data:count acc ->
+      if stone = 0
+      then (
+        let prev = get_or_default acc 1 in
+        Map.set acc ~key:1 ~data:(count + prev))
+      else if String.length (Int.to_string stone) % 2 = 0
+      then (
+        let left, right = split_stone (Int.to_string stone) in
+        let prev_left = get_or_default acc left in
+        let left_added = Map.set acc ~key:left ~data:(prev_left + count) in
+        let prev_right = get_or_default left_added right in
+        Map.set left_added ~key:right ~data:(prev_right + count))
+      else (
+        let new_stone = stone * 2024 in
+        let prev = get_or_default acc new_stone in
+        Map.set acc ~key:new_stone ~data:(prev + count)))
+  in
+  created
 ;;
 
 let count_stones list =
   List.fold
     list
-    ~init:(Map.empty (module String))
+    ~init:(Map.empty (module Int))
     ~f:(fun acc x ->
       match Map.find acc x with
       | Some prev -> Map.set acc ~key:x ~data:(prev + 1)
       | None -> Map.set acc ~key:x ~data:1)
 ;;
 
-let change_and_collect stone_counts =
-  Map.fold
-    stone_counts
-    ~init:(Map.empty (module String))
-    ~f:(fun ~key ~data acc ->
-      let stones_created =
-        Fn.apply_n_times ~n:25 (fun x -> change_stones [] x) [ key ]
-      in
-      let new_stone_count = count_stones stones_created in
-      Map.merge acc new_stone_count ~f:(fun ~key ->
-          function
-          | `Left value ->
-            ignore key;
-            Some value
-          | `Right value ->
-            ignore key;
-            Some (value * data)
-          | `Both (left, right) ->
-            ignore key;
-            Some (left + (data * right))))
+let part_one stones =
+  let init = count_stones stones in
+  let result = Fn.apply_n_times ~n:25 blink init in
+  Map.data result |> List.fold ~init:0 ~f:(fun acc x -> acc + x)
 ;;
 
 let part_two stones =
-  let init_count = count_stones stones in
-  let result = Fn.apply_n_times ~n:3 change_and_collect init_count in
+  let init = count_stones stones in
+  let result = Fn.apply_n_times ~n:75 blink init in
   Map.data result |> List.fold ~init:0 ~f:(fun acc x -> acc + x)
 ;;
 
 let run file part =
   let tokens =
-    In_channel.read_all file |> String.strip |> String.split ~on:' '
+    In_channel.read_all file
+    |> String.strip
+    |> String.split ~on:' '
+    |> List.map ~f:Int.of_string
   in
   if part = 1 then part_one tokens else part_two tokens
 ;;
